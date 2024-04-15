fba2908eb5c92723027a35b12fa2c398c16d1ee3
Md-Asikur <asikurrahaman997@gamil.com>
2024-04-15 12:31:12 +0600

    x

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -3,14 +3,14 @@ import { getProductClinet } from "@/utility/getData";
 import { headers } from "next/headers";
 
 async function page() {
-  // let { data } = await getProductClinet();
-  // data = data.slice(0, 3);
+  let { data } = await getProductClinet();
+  data = data.slice(0, 3);
 
   // const headersList = headers();
   // const token = headersList.get("");
   // console.log(token, " token cart data");
 
-  return <>cart</>; //<Cart data={data} />
+  return <Cart data={data} />;
 }
 
 export default page;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,12 +3,12 @@ import { getProductClinet } from "@/utility/getData";
 import React from "react";
 
 async function Page() {
-  // const { data } = await getProductClinet();
+  const { data } = await getProductClinet();
 
   return (
     <div className=" container py-20">
       <h1 className=" text-[30px]"> All Product </h1>
-      {/* <Card data={data} /> */}
+      <Card data={data} />
 
       <ol className="flex justify-center gap-1 text-xs font-medium">
         <li>
